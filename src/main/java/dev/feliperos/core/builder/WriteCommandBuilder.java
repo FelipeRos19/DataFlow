8fa9fe4ace620b30a5e75f43dd61e1452aeba764
Felipe <feliperos.simao@gmail.com>
2024-03-04 16:27:45 -0300

    feat(commands): initial implementation of abstract write action.

--- src/main/java/dev/feliperos/core/builder/WriteCommandBuilder.java
+++ src/main/java/dev/feliperos/core/builder/WriteCommandBuilder.java
@@ -1,22 +1,22 @@
 package dev.feliperos.core.builder;
 
+import dev.feliperos.core.base.actions.Write;
 import dev.feliperos.core.base.structs.Execute;
-import dev.feliperos.core.base.actions.Read;
 
 /**
- * Construtor abstrato de um comando de leitura no Redis.
+ * Construtor abstrato de um comando de escrita no Redis.
  *
- * @see Read
+ * @see Write
  * @see Execute
  *
- * @author Felipe, Felipe Ros. Created on 01/03/2024.
+ * @author Felipe, Felipe Ros. Created on 04/03/2024.
  * @since 1.0
  * @version 1.0
  *
- * @param <K> classe do comando.
+ * @param <K> classe de comando.
  * @param <T> tipo de retorno da execução.
  */
-public abstract class ReadCommandBuilder<K, T> implements Read<K>, Execute<T> {
+public abstract class WriteCommandBuilder<K, T> implements Write<K>, Execute<T> {
 
     /**
      * Constrói o Comando e retorna configurado.
