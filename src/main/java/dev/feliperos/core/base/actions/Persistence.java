3a4d57f88ac996d5863e986a3ce9c4b9d3c87a8b
Felipe <feliperos.simao@gmail.com>
2024-03-28 17:40:03 -0300

    docs(actions): updating interface parameters.

--- src/main/java/dev/feliperos/core/base/actions/Persistence.java
+++ src/main/java/dev/feliperos/core/base/actions/Persistence.java
@@ -1,5 +1,14 @@
 package dev.feliperos.core.base.actions;
 
+/**
+ * Representação a Ação de Persistir uma Chave dentro do Redis.
+ *
+ * @author Felipe, Felipe Ros. Creadted on 28/03/2024
+ * @since 1.0
+ * @version 1.0
+ *
+ * @param <T> Classe que implementa.
+ */
 public interface Persistence<T> {
 
     /**
