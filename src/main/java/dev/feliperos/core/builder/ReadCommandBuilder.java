079b4499b43be3a318cd8ca1003245f40cece1ac
Felipe <feliperos.simao@gmail.com>
2024-04-01 22:58:34 -0300

    refactor(builder): using new build action.

--- src/main/java/dev/feliperos/core/builder/ReadCommandBuilder.java
+++ src/main/java/dev/feliperos/core/builder/ReadCommandBuilder.java
@@ -1,27 +1,22 @@
 package dev.feliperos.core.builder;
 
+import dev.feliperos.core.base.structs.Build;
 import dev.feliperos.core.base.structs.Execute;
 import dev.feliperos.core.base.actions.string.Read;
 
 /**
  * Construtor abstrato de um comando de leitura no Redis.
  *
- * @see Read
- * @see Execute
+ * @see dev.feliperos.core.base.actions.string.Read
+ * @see dev.feliperos.core.base.structs.Execute
+ * @see dev.feliperos.core.base.structs.Build
  *
  * @author Felipe, Felipe Ros. Created on 01/03/2024.
  * @since 1.0
- * @version 1.0
+ * @version 1.1.0
  *
  * @param <K> classe do comando.
  * @param <T> tipo de retorno da execução.
  */
-public abstract class ReadCommandBuilder<K, T> implements Read<K>, Execute<T> {
-
-    /**
-     * Constrói o Comando e retorna configurado.
-     *
-     * @return comando construído.
-     */
-    public abstract K build();
+public abstract class ReadCommandBuilder<K, T> implements Read<K>, Execute<T>, Build<K> {
 }
--- src/main/java/dev/feliperos/core/builder/WriteCommandBuilder.java
+++ src/main/java/dev/feliperos/core/builder/WriteCommandBuilder.java
@@ -1,27 +1,22 @@
 package dev.feliperos.core.builder;
 
 import dev.feliperos.core.base.actions.string.Write;
+import dev.feliperos.core.base.structs.Build;
 import dev.feliperos.core.base.structs.Execute;
 
 /**
  * Construtor abstrato de um comando de escrita no Redis.
  *
- * @see Write
- * @see Execute
+ * @see dev.feliperos.core.base.actions.string.Write
+ * @see dev.feliperos.core.base.structs.Execute
+ * @see dev.feliperos.core.base.structs.Build
  *
  * @author Felipe, Felipe Ros. Created on 04/03/2024.
  * @since 1.0
- * @version 1.0
+ * @version 1.1.0
  *
  * @param <K> classe de comando.
  * @param <T> tipo de retorno da execução.
  */
-public abstract class WriteCommandBuilder<K, T> implements Write<K>, Execute<T> {
-
-    /**
-     * Constrói o Comando e retorna configurado.
-     *
-     * @return comando construído.
-     */
-    public abstract K build();
+public abstract class WriteCommandBuilder<K, T> implements Write<K>, Execute<T>, Build<K> {
 }
