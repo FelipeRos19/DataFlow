134d0c39385ef834bc027beae84b69accdebe38b
Felipe <feliperos.simao@gmail.com>
2024-04-02 20:23:07 -0300

    docs(struct): new multiValue docs.

--- src/main/java/dev/feliperos/core/base/structs/MultiValue.java
+++ src/main/java/dev/feliperos/core/base/structs/MultiValue.java
@@ -1,6 +1,21 @@
 package dev.feliperos.core.base.structs;
 
+/**
+ * Representação da Estrutura de definir Múltiplos Valores dentro do Redis.
+ *
+ * @author Felipe, Felipe Ros. Creadted on 01/04/2024
+ * @since 1.0
+ * @version 1.0
+ *
+ * @param <T> Classe que implementa.
+ */
 public interface MultiValue<T> {
 
+    /**
+     * Utilizando para definir os valores de inserção.
+     *
+     * @param values valores de inserção.
+     * @return T objeto em construção.
+     */
     T setValues(String... values);
 }
